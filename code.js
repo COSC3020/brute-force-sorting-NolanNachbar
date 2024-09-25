8aee7fb737c964cc35e5b20b76dbab85a01e9389
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-09-25 16:19:21 -0600

    Update code.js

--- code.js
+++ code.js
@@ -7,27 +7,28 @@ function permutationSort(a) {
         }
         return true;
     }
-    
+
     function swap(arr, i, j) {
         let temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
-    
+
     function permute(arr, n) {
-        if (solved(arr)) {
+        if (n === arr.length - 1) {
             count++;
             return;
         }
-    
+
         for (let i = n; i < arr.length; i++) {
             swap(arr, n, i);
             permute(arr, n + 1);
+            if(solved(arr)) return;
             swap(arr, n, i);
         }
     }
 
     permute(a, 0);
-    
+
     return count; 
 }
